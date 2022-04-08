475dc66029dcd1cd306c9187829b1c38374e0293
Murillo Nogueira <murillo.nogueira@qdois.com.br>
2022-04-07 23:19:27 -0300

    swagger

--- TvCinema.MovieApi.Service.Api/Program.cs
+++ TvCinema.MovieApi.Service.Api/Program.cs
@@ -10,15 +10,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddMapper();
+builder.Services.AddServices();
+builder.Services.AddRepositories();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
---
+++ TvCinema.MovieApi.Service.CrossCutting/IoC/RepositoryConfig.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TvCinema.MovieApi.Service.Domain.Interfaces;
+using TvCinema.MovieApi.Service.Infrastructure.Repositories;
+
+namespace TvCinema.MovieApi.Service.CrossCutting.IoC
+{
+    public static class RepositoryConfig
+    {
+        public static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IMovieRepository, MovieRepository>();
+        }
+    }
+}
